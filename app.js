f1d2643c5b3882aa968b86bc9181045672487d2e
vishal khilari <vishalkhilari0@gmail.com>
2025-10-08 19:59:17 +0530

    Element selector of HTML using JS

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log(document.body);
+console.log(document.getElementById("subheading"));
+console.log(document.getElementsByTagName("h4"));
+console.log(document.getElementsByClassName("mainheading"));
+console.log(document.querySelector(".mainheading"));
+console.log(document.querySelectorAll(".mainheading"));
