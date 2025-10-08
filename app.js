1f914b7a4b39a0c926a951ad0d25cfcc0246c29e
vishal khilari <vishalkhilari0@gmail.com>
2025-10-08 20:59:40 +0530

    Ways to view content between tags

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-console.log(document.getElementById("subheading"));
-console.log(document.getElementsByTagName("h4"));
-console.log(document.getElementsByClassName("mainheading"));
-console.log(document.querySelector(".mainheading"));
-console.log(document.querySelectorAll(".mainheading"));
+function print(a) {
+  console.log(a);
+}
+
+const p = document.querySelector("p");
+print(p.innerText);
+print(p.textContent);
+print(p.innerHTML);
