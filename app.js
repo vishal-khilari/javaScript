4d425a8de5a1acbe5a71f6289b1f48d68077c624
vishal khilari <vishalkhilari0@gmail.com>
2025-10-09 19:19:47 +0530

    Adding element to html whever we want.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const h1 = document.querySelector("h1");
 
-const h2 = document.createElement("h2"); // create <h2>
-h2.textContent = "hiiii"; // set text
-h2.classList.add("heading");
-document.body.appendChild(h2); // append to body
+const h3 = document.createElement("h3");
+h3.innerText = "insert element";
+h3.style.color = "teal";
+document.body.appendChild(h3);
+h1.insertAdjacentElement("beforebegin", h3);
