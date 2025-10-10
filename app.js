55275eb655ba66ad8414035878d74f0fc44d1638
vishal khilari <vishalkhilari0@gmail.com>
2025-10-10 22:22:27 +0530

    Function on input of form.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-function print() {
-  console.log("Hello World");
-}
-const form = document.querySelector("form");
+const form = document.querySelector(".form1");
 const input = document.querySelector("input");
 
-form.addEventListener("submit", (event) => {
+function istrueorfalse(inputvalue) {
+  if (inputvalue >= 10) {
+    console.log("input is greater than 10");
+  } else {
+    console.log("input is lesser than 10");
+  }
+}
+
+const inputvalue = form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log(input.value);
+  istrueorfalse(input.value);
 });
