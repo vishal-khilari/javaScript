c169415e240a1582966e60293a6106d3a2132f9f
vishal khilari <vishalkhilari0@gmail.com>
2025-10-10 23:32:43 +0530

    Mouct events.

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const form = document.querySelector(".form1");
-const input = document.querySelector("input");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let ten = document.querySelector(".ten");
 
-function istrueorfalse(inputvalue) {
-  if (inputvalue >= 10) {
-    console.log("input is greater than 10");
-  } else {
-    console.log("input is lesser than 10");
-  }
-}
-
-const inputvalue = form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log(input.value);
-  istrueorfalse(input.value);
+one.addEventListener("click", () => {
+  one.classList.toggle("red");
+});
+two.addEventListener("contextmenu", () => {
+  two.classList.toggle("red");
+});
+three.addEventListener("dblclick", () => {
+  three.classList.toggle("red");
+});
+four.addEventListener("mousedown", () => {
+  four.classList.toggle("red");
+});
+five.addEventListener("mouseenter", () => {
+  five.classList.toggle("red");
+});
+six.addEventListener("mouseleave", () => {
+  six.classList.toggle("red");
+});
+seven.addEventListener("mousemove", () => {
+  seven.classList.toggle("red");
+});
+eight.addEventListener("mouseout", () => {
+  eight.classList.toggle("red");
+});
+nine.addEventListener("mouseover", () => {
+  nine.classList.toggle("red");
+});
+ten.addEventListener("mouseup", () => {
+  ten.classList.toggle("red");
 });
