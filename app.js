cd068013216bb045f37328217ed0fee35309bcf0
vishal khilari <vishalkhilari0@gmail.com>
2025-10-11 22:50:21 +0530

    Keyboard events

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const input = document.querySelector("input");
 
-input.addEventListener("keypress", (e) => console.log(e.charCode));
+input.addEventListener("keypress", (e) =>
+  // <---use only one "console.log at a time"--->
+  // console.log(e.charCode)
+  // console.log(e.code)
+  // console.log(e.ctrlKey)
+  // console.log(e.key)
+  // console.log(e.shiftKey)
+);
